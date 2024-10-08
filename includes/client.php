3d5efd988388f370bd9f64ba58e60531b5775184
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2024-10-08 21:35:12 +0200

    PHPCS, input sanitization and output escaping (#1)
    To publish the plugin to the WordPress.org repo, you have to check it with the plugin check https://de.wordpress.org/plugins/plugin-check/
    
    This PR should fix all of the issues

--- includes/client.php
+++ includes/client.php
@@ -4,70 +4,75 @@ namespace Subclub;
 
 use Exception;
 
-class Client
-{
-    private $apiUrl = 'https://api.sub.club/public';
-    private $apiKey;
+class Client {
 
-    public function __construct($apiKey)
-    {
-        $this->apiKey = $apiKey;
-    }
+	private $apiUrl = 'https://api.sub.club/public';
+	private $apiKey;
 
-    // creates a new post on sub.club with the given parameters.
-    public function createPost($params)
-    {
-        $response = wp_remote_request("{$this->apiUrl}/post", [
-            'method' => 'POST',
-            'headers' => [
-                'Content-Type' => 'application/json',
-                'Authorization' => "Bearer {$this->apiKey}",
-            ],
-            'body' => json_encode($params),
-        ]);
+	public function __construct( $apiKey ) {
+		$this->apiKey = $apiKey;
+	}
 
-        if (is_wp_error($response)) {
-            throw new Exception("Failed to create post: " . $response->get_error_message());
-        }
+	// creates a new post on sub.club with the given parameters.
+	public function createPost( $params ) {
+		$response = wp_remote_request(
+			"{$this->apiUrl}/post",
+			array(
+				'method'  => 'POST',
+				'headers' => array(
+					'Content-Type'  => 'application/json',
+					'Authorization' => "Bearer {$this->apiKey}",
+				),
+				'body'    => wp_json_encode( $params ),
+			)
+		);
 
-        return json_decode($response['body'], true);
-    }
+		if ( is_wp_error( $response ) ) {
+			throw new Exception( 'Failed to create post: ' . esc_html( $response->get_error_message() ) );
+		}
 
-    // edits the given post with the supplied PostUpdateParams.
-    public function editPost($params)
-    {
-        $response = wp_remote_request("{$this->apiUrl}/post/edit", [
-            'method' => 'PUT',
-            'headers' => [
-                'Content-Type' => 'application/json',
-                'Authorization' => "Bearer {$this->apiKey}",
-            ],
-            'body' => json_encode($params),
-        ]);
+		return json_decode( $response['body'], true );
+	}
 
-        if (is_wp_error($response)) {
-            throw new Exception("Failed to create post: " . $response->get_error_message());
-        }
+	// edits the given post with the supplied PostUpdateParams.
+	public function editPost( $params ) {
+		$response = wp_remote_request(
+			"{$this->apiUrl}/post/edit",
+			array(
+				'method'  => 'PUT',
+				'headers' => array(
+					'Content-Type'  => 'application/json',
+					'Authorization' => "Bearer {$this->apiKey}",
+				),
+				'body'    => wp_json_encode( $params ),
+			)
+		);
 
-        return json_decode($response['body'], true);
-    }
+		if ( is_wp_error( $response ) ) {
+			throw new Exception( 'Failed to create post: ' . esc_html( $response->get_error_message() ) );
+		}
 
-    // deletes a post with the given postID.
-    public function deletePost($params)
-    {
-        $response = wp_remote_request("{$this->apiUrl}/post/delete", [
-            'method' => 'DELETE',
-            'headers' => [
-                'Content-Type' => 'application/json',
-                'Authorization' => "Bearer {$this->apiKey}",
-            ],
-            'body' => json_encode($params),
-        ]);
+		return json_decode( $response['body'], true );
+	}
 
-        if (is_wp_error($response)) {
-            throw new Exception("Failed to create post: " . $response->get_error_message());
-        }
+	// deletes a post with the given postID.
+	public function deletePost( $params ) {
+		$response = wp_remote_request(
+			"{$this->apiUrl}/post/delete",
+			array(
+				'method'  => 'DELETE',
+				'headers' => array(
+					'Content-Type'  => 'application/json',
+					'Authorization' => "Bearer {$this->apiKey}",
+				),
+				'body'    => wp_json_encode( $params ),
+			)
+		);
 
-        return json_decode($response['body'], true);
-    }
+		if ( is_wp_error( $response ) ) {
+			throw new Exception( 'Failed to create post: ' . esc_html( $response->get_error_message() ) );
+		}
+
+		return json_decode( $response['body'], true );
+	}
 }
--- includes/publish.php
+++ includes/publish.php
@@ -4,168 +4,163 @@ namespace Subclub;
 
 use Exception;
 
-class Publish
-{
-    public static function init()
-    {
-        add_action('wp_trash_post', array(__CLASS__, 'subclub_delete_premium_post'));
-        add_action('add_meta_boxes', array(__CLASS__, 'subclub_add_meta_box'), 10, 2);
-        add_action('save_post', array(__CLASS__, 'subclub_save_meta_box_data'));
-    }
-    // Add meta box to the post editor
-    static function subclub_add_meta_box()
-    {
-        add_meta_box(
-            'subclub_post_options',
-            'sub.club',
-            array(__CLASS__, 'subclub_meta_box_callback'),
-            'post',
-            'side',
-        );
-    }
+class Publish {
 
-    // Meta box callback function
-    static function subclub_meta_box_callback($post)
-    {
-        wp_nonce_field('subclub_save_meta_box_data', 'subclub_meta_box_nonce');
-        $post_type_value = get_post_meta($post->ID, '_subclub_post_type', true);
-        if (empty($post_type_value)) {
-            $post_type_value = 'premium';
-        }
-        $delete_option_value = get_post_meta($post->ID, '_subclub_delete_premium', true);
-?>
-        <p>
-            <label for="subclub_post_type">Post type:</label>
-            <select name="subclub_post_type" id="subclub_post_type">
-                <option value="free" <?php selected($post_type_value, 'free'); ?>>Free</option>
-                <option value="premium" <?php selected($post_type_value, 'premium'); ?>>Premium</option>
-            </select>
-        </p>
-        <p>
-            <label for="subclub_delete_premium_option"><?php _e('On delete', 'subclub'); ?></label>
-            <select name="subclub_delete_premium_option" id="subclub_delete_premium_option">
-                <option value="delete" <?php selected($delete_option_value, 'delete'); ?>><?php _e('Delete premium', 'subclub'); ?></option>
-                <option value="keep" <?php selected($delete_option_value, 'keep'); ?>><?php _e('Keep premium', 'subclub'); ?></option>
-            </select>
-        </p>
-        <?php
-    }
+	public static function init() {
+		add_action( 'wp_trash_post', array( __CLASS__, 'subclub_delete_premium_post' ) );
+		add_action( 'add_meta_boxes', array( __CLASS__, 'subclub_add_meta_box' ), 10, 2 );
+		add_action( 'save_post', array( __CLASS__, 'subclub_save_meta_box_data' ) );
+	}
+	// Add meta box to the post editor
+	static function subclub_add_meta_box() {
+		add_meta_box(
+			'subclub_post_options',
+			'sub.club',
+			array( __CLASS__, 'subclub_meta_box_callback' ),
+			'post',
+			'side',
+		);
+	}
 
-    // Save meta box data
-    static function subclub_save_meta_box_data($post_id)
-    {
-        if (!isset($_POST['subclub_meta_box_nonce'])) {
-            return;
-        }
-        if (!wp_verify_nonce($_POST['subclub_meta_box_nonce'], 'subclub_save_meta_box_data')) {
-            return;
-        }
-        if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
-            return;
-        }
-        if (!current_user_can('edit_post', $post_id)) {
-            return;
-        }
+	// Meta box callback function
+	static function subclub_meta_box_callback( $post ) {
+		wp_nonce_field( 'subclub_save_meta_box_data', 'subclub_meta_box_nonce' );
+		$post_type_value = get_post_meta( $post->ID, '_subclub_post_type', true );
+		if ( empty( $post_type_value ) ) {
+			$post_type_value = 'premium';
+		}
+		$delete_option_value = get_post_meta( $post->ID, '_subclub_delete_premium', true );
+		?>
+		<p>
+			<label for="subclub_post_type">Post type:</label>
+			<select name="subclub_post_type" id="subclub_post_type">
+				<option value="free" <?php selected( $post_type_value, 'free' ); ?>>Free</option>
+				<option value="premium" <?php selected( $post_type_value, 'premium' ); ?>>Premium</option>
+			</select>
+		</p>
+		<p>
+			<label for="subclub_delete_premium_option"><?php esc_html_e( 'On delete', 'subclub' ); ?></label>
+			<select name="subclub_delete_premium_option" id="subclub_delete_premium_option">
+				<option value="delete" <?php selected( $delete_option_value, 'delete' ); ?>><?php esc_html_e( 'Delete premium', 'subclub' ); ?></option>
+				<option value="keep" <?php selected( $delete_option_value, 'keep' ); ?>><?php esc_html_e( 'Keep premium', 'subclub' ); ?></option>
+			</select>
+		</p>
+		<?php
+	}
 
-        if (!isset($_POST['subclub_post_type'])) {
-            return;
-        }
+	// Save meta box data
+	static function subclub_save_meta_box_data( $post_id ) {
+		if ( ! isset( $_POST['subclub_meta_box_nonce'] ) ) {
+			return;
+		}
+		if ( ! wp_verify_nonce( sanitize_text_field( wp_unslash( $_POST['subclub_meta_box_nonce'] ) ), 'subclub_save_meta_box_data' ) ) {
+			return;
+		}
+		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+			return;
+		}
+		if ( ! current_user_can( 'edit_post', $post_id ) ) {
+			return;
+		}
 
-        if (array_key_exists('subclub_delete_premium', $_POST)) {
-            update_post_meta(
-                $post_id,
-                '_subclub_delete_premium',
-                $_POST['subclub_delete_premium']
-            );
-        } else {
-            delete_post_meta($post_id, '_subclub_delete_premium');
-        }
+		if ( ! isset( $_POST['subclub_post_type'] ) ) {
+			return;
+		}
 
-        $post_type = sanitize_text_field($_POST['subclub_post_type']);
-        update_post_meta($post_id, '_subclub_post_type', $post_type);
+		if ( array_key_exists( 'subclub_delete_premium', $_POST ) ) {
+			update_post_meta(
+				$post_id,
+				'_subclub_delete_premium',
+				sanitize_text_field( wp_unslash( $_POST['subclub_delete_premium'] ) )
+			);
+		} else {
+			delete_post_meta( $post_id, '_subclub_delete_premium' );
+		}
 
-        if (!isset($_POST['subclub_delete_premium_option'])) {
-            return;
-        }
+		$post_type = sanitize_text_field( wp_unslash( $_POST['subclub_post_type'] ) );
+		update_post_meta( $post_id, '_subclub_post_type', $post_type );
 
-        $delete_option = sanitize_text_field($_POST['subclub_delete_premium_option']);
-        update_post_meta($post_id, '_subclub_delete_premium', $delete_option);
+		if ( ! isset( $_POST['subclub_delete_premium_option'] ) ) {
+			return;
+		}
 
-        if ($post_type == 'premium') {
-            $title = get_the_title($post_id);
-            $post = get_post($post_id);
-            $excerpt = $post->post_excerpt;
-            $post_content = '<h1>' . $title . '</h1>' . $post->post_content;
+		$delete_option = sanitize_text_field( wp_unslash( $_POST['subclub_delete_premium_option'] ) );
+		update_post_meta( $post_id, '_subclub_delete_premium', $delete_option );
 
-            if (!$excerpt) {
-                $excerpt = 'New premium post';
-            }
+		if ( $post_type == 'premium' ) {
+			$title        = get_the_title( $post_id );
+			$post         = get_post( $post_id );
+			$excerpt      = $post->post_excerpt;
+			$post_content = '<h1>' . $title . '</h1>' . $post->post_content;
 
-            // Post to SubClub
-            $api_key = get_option('subclub_api_key');
+			if ( ! $excerpt ) {
+				$excerpt = 'New premium post';
+			}
 
-            if ($api_key) {
-                $subclub = new Client($api_key);
-                $post = get_post($post_id);
+			// Post to SubClub
+			$api_key = get_option( 'subclub_api_key' );
 
-                try {
-                    $params = ['content' => $post_content];
-                    $response = $subclub->createPost($params);
+			if ( $api_key ) {
+				$subclub = new Client( $api_key );
+				$post    = get_post( $post_id );
 
-                    if (isset($response['url'])) {
-                        // Store the URL in post meta
-                        update_post_meta($post_id, '_subclub_post_url', $response['url']);
-                        update_post_meta($post_id, '_subclub_post_id', $response['postId']);
+				try {
+					$params   = array( 'content' => $post_content );
+					$response = $subclub->createPost( $params );
 
-                        // Concat the excerpt with the subclub post URL
-                        $excerpt = $excerpt . '<br /><br />RE: <a href="' . $response['url'] . '" target="_blank">' . $response['url'] . '</a>';
+					if ( isset( $response['url'] ) ) {
+						// Store the URL in post meta
+						update_post_meta( $post_id, '_subclub_post_url', $response['url'] );
+						update_post_meta( $post_id, '_subclub_post_id', $response['postId'] );
 
-                        // Hacky way to prevent infinite recursion
-                        unset($_POST['subclub_meta_box_nonce']);
+						// Concat the excerpt with the subclub post URL
+						$excerpt = $excerpt . '<br /><br />RE: <a href="' . $response['url'] . '" target="_blank">' . $response['url'] . '</a>';
 
-                        // Update post body with the excerpt
-                        wp_update_post(
-                            array(
-                                'ID' => $post_id,
-                                'post_content' => $excerpt,
-                                'meta_input' => array(
-                                    '_subclub_post_id' => $response['postId'],
-                                    '_subclub_post_url' => $response['url'],
-                                ),
-                            )
-                        );
+						// Hacky way to prevent infinite recursion
+						unset( $_POST['subclub_meta_box_nonce'] );
 
-                        // update the post composer content with the content of $excerpt. Use jquery to do this.
-        ?>
-                        <script>
-                            jQuery(document).ready(function($) {
-                                $('#content').val('<?php echo esc_js($excerpt); ?>');
-                            });
-                        </script>
-<?php
-                    }
-                } catch (Exception $e) {
-                    // Handle any errors (e.g., log them or notify the admin)
-                    error_log('SubClub API Error: ' . $e->getMessage());
-                }
-            }
-        }
-    }
+						// Update post body with the excerpt
+						wp_update_post(
+							array(
+								'ID'           => $post_id,
+								'post_content' => $excerpt,
+								'meta_input'   => array(
+									'_subclub_post_id'  => $response['postId'],
+									'_subclub_post_url' => $response['url'],
+								),
+							)
+						);
 
-    static function subclub_delete_premium_post($post_id)
-    {
-        $delete_premium = get_post_meta($post_id, '_subclub_delete_premium', true);
-        $subclub_post_id = get_post_meta($post_id, '_subclub_post_id', true);
+						// update the post composer content with the content of $excerpt. Use jquery to do this.
+						?>
+						<script>
+							jQuery(document).ready(function($) {
+								$('#content').val('<?php echo esc_js( $excerpt ); ?>');
+							});
+						</script>
+						<?php
+					}
+				} catch ( Exception $e ) {
+					// Handle any errors (e.g., log them or notify the admin)
+					error_log( 'SubClub API Error: ' . $e->getMessage() );
+				}
+			}
+		}
+	}
 
-        if ($delete_premium == 'delete' && isset($subclub_post_id)) {
-            $api_key = get_option('subclub_api_key');
-            $subclub = new Client($api_key);
-            $params = ['postId' => $subclub_post_id];
-            try {
-                $subclub->deletePost($params);
-            } catch (Exception $e) {
-                error_log('SubClub API Error: ' . $e->getMessage());
-            }
-        }
-    }
+	static function subclub_delete_premium_post( $post_id ) {
+		$delete_premium  = get_post_meta( $post_id, '_subclub_delete_premium', true );
+		$subclub_post_id = get_post_meta( $post_id, '_subclub_post_id', true );
+
+		if ( $delete_premium == 'delete' && isset( $subclub_post_id ) ) {
+			$api_key = get_option( 'subclub_api_key' );
+			$subclub = new Client( $api_key );
+			$params  = array( 'postId' => $subclub_post_id );
+			try {
+				$subclub->deletePost( $params );
+			} catch ( Exception $e ) {
+				error_log( 'SubClub API Error: ' . $e->getMessage() );
+			}
+		}
+	}
 }
