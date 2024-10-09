e2b9c78d79f96a5c52458c32f02f5a9524c5d581
Benoit Nolens <bnolens@gmail.com>
2024-10-09 17:58:45 +0200

    feat: extract images/videos and include in AP attachment

--- includes/publish.php
+++ includes/publish.php
@@ -104,8 +104,19 @@ class Publish {
 				$subclub = new Client( $api_key );
 				$post    = get_post( $post_id );
 
+				$media_attachments = self::get_post_images_and_videos( $post_id );
+
+				// Remove all images and videos from the post content
+				$post_content = preg_replace('/<img[^>]*>/i', '', $post_content);
+				$post_content = preg_replace('/<video[^>]*>.*?<\/video>/i', '', $post_content);
+
+				// Sanitize the content to regular HTML
+				$post_content = wp_kses_post($post_content);
+				// Remove empty tags from the $post_content HTML
+				$post_content = preg_replace('/<(\w+)\b(?:\s+[^>]*)?>\s*<\/\1>/', '', $post_content);
+
 				try {
-					$params   = array( 'content' => $post_content );
+					$params   = array( 'content' => $post_content, 'attachments' => $media_attachments );
 					$response = $subclub->createPost( $params );
 
 					if ( isset( $response['url'] ) ) {
@@ -162,5 +173,91 @@ class Publish {
 				error_log( 'SubClub API Error: ' . $e->getMessage() );
 			}
 		}
+	}
+
+	static function get_post_images_and_videos( $post_id ) {
+		// Get the post object.
+		$post = get_post( $post_id );
+		if ( ! $post ) {
+			return array(); // Return an empty array if the post doesn't exist.
+		}
+	
+		// Array to hold media information (image and video objects).
+		$media_objects = array();
+	
+		// 1. Get attached images and videos using MIME types
+		// Get attached images.
+		$attached_images = get_attached_media( 'image', $post_id );
+		// Get attached videos.
+		$attached_videos = get_attached_media( 'video', $post_id );
+	
+		// Helper function to get media details.
+		function get_media_details( $attachment_id ) {
+			// Get the attachment's metadata.
+			$attachment_meta = wp_get_attachment_metadata( $attachment_id );
+			$media_url = wp_get_attachment_url( $attachment_id );
+			$mime_type = get_post_mime_type( $attachment_id );
+	
+			// Default dimensions in case metadata is missing.
+			$width = isset( $attachment_meta['width'] ) ? $attachment_meta['width'] : 0;
+			$height = isset( $attachment_meta['height'] ) ? $attachment_meta['height'] : 0;
+	
+			// For videos, try to grab the width/height from video metadata.
+			if ( strpos( $mime_type, 'video' ) !== false && isset( $attachment_meta['sizes'] ) ) {
+				$width = isset( $attachment_meta['sizes']['width'] ) ? $attachment_meta['sizes']['width'] : $width;
+				$height = isset( $attachment_meta['sizes']['height'] ) ? $attachment_meta['sizes']['height'] : $height;
+			}
+	
+			// Return an object with the necessary details.
+			return (object) array(
+				'width'     => $width,
+				'height'    => $height,
+				'url'       => $media_url,
+				'mediaType' => $mime_type,
+				'type' 		=> 'Document'
+			);
+		}
+	
+		// 2. Loop through attached images and videos, adding their details to the array.
+		foreach ( $attached_images as $image ) {
+			$media_objects[] = get_media_details( $image->ID );
+		}
+		foreach ( $attached_videos as $video ) {
+			$media_objects[] = get_media_details( $video->ID );
+		}
+	
+		// 3. Extract media (images & videos) from the post content itself.
+		// Find all image IDs in post content.
+		if ( preg_match_all( '/wp-image-(\d+)/', $post->post_content, $matches ) ) {
+			foreach ( $matches[1] as $image_id ) {
+				$media_objects[] = get_media_details( intval( $image_id ) );
+			}
+		}
+	
+		// Find all video IDs in post content (if inserted as a wp-video class).
+		if ( preg_match_all( '/<video.*?wp-video-(\d+)/', $post->post_content, $matches ) ) {
+			foreach ( $matches[1] as $video_id ) {
+				$media_objects[] = get_media_details( intval( $video_id ) );
+			}
+		}
+	
+		// Handle [video] shortcodes in the post content (embedded videos).
+		if ( has_shortcode( $post->post_content, 'video' ) ) {
+			$pattern = get_shortcode_regex( array( 'video' ) );
+			if ( preg_match_all( '/' . $pattern . '/s', $post->post_content, $matches ) && isset( $matches[3] ) ) {
+				foreach ( $matches[3] as $attrs ) {
+					$attrs = shortcode_parse_atts( $attrs );
+					if ( isset( $attrs['ids'] ) ) {
+						$ids = explode( ',', $attrs['ids'] );
+						foreach ( $ids as $video_id ) {
+							$media_objects[] = get_media_details( intval( $video_id ) );
+						}
+					}
+				}
+			}
+		}
+	
+		// Return the array of media objects.
+		return $media_objects;
 	}
 }
